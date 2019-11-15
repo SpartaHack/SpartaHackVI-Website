f35eac2b261730d8a1eaec77772c74a8c1761aa6
Jarek Troyer <jarektroyer@gmail.com>
2019-11-14 20:12:18 -0500

    Get university autocomplete setup

--- src/js/applications/autocomplete/directory.js
+++ src/js/applications/autocomplete/directory.js
@@ -1,13 +1,15 @@
 let autoDOM = (element, options) => {
-    console.log(element)
+    console.log(options)
+    // console.log(element)
 }
 let autoFuncs = {
-    'travel-origin': require('./travelOrigin'),
-    'university': require('./university'),
-    'major': require('./major')
+    'travel-origin': require('./travelOrigin').default,
+    'university': require('./university').default,
+    'major': require('./major').default
 }
 module.exports.default = element => {
     let af = autoFuncs[element.id]
+    
     if (af) element.addEventListener('keyup', 
-        e => autoDOM(element, af(element.value)) )
+        e => autoDOM(element, af(element)) )
 }
--- src/js/applications/autocomplete/university.js
+++ src/js/applications/autocomplete/university.js
@@ -1,3 +1,14 @@
-module.exports = src => {
-    
+const uniList = require('./../../uni-list.json')
+
+module.exports.default = element => {
+    let val = element.value.toLowerCase()
+    let matching = []
+
+    // console.log(uniList)
+    uniList.forEach(uni => {
+        if (uni['name'].toLowerCase().indexOf(val) != -1)
+            matching.push(uni['name'])
+    })
+
+    return matching
 }
