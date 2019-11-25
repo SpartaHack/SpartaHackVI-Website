48f2938af113bf19fec0d3f9e5bfe4b8e26c2c20
Jarek Troyer <jarektroyer@gmail.com>
2019-11-25 17:08:16 -0500

    redirect authentication now works

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,7 +1,11 @@
 const auth = require('./auth_cofig').default
 
 let testing = async auth0=> {
-    console.log(auth0)
+    let f = await auth0.handleRedirectCallback()
+    let y = await auth0.getUser()
+    console.log(y)
+
+    // console.log(auth0)
 }
 
 auth(testing)
