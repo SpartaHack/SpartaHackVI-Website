64652a5c60dcb311586739710dc43e52e20d2b89
Jarek Troyer <jarektroyer@gmail.com>
2019-11-25 15:32:59 -0500

    MLH Branding

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,7 @@ auth(
     authObj => document.getElementById('nav-login')
     .addEventListener('click', () => authObj.loginWithPopup())
 )
+
+let countdown = document.getElementById('countdown')
+
+countdown.innerHTML = Math.floor((new Date('12/1/19')- new Date())/(1000*3600*24)).toString() + countdown.innerHTML
