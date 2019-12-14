b30a114527c65139bdb3a0c9e67ced39dbe0c2ea
Jarek Troyer <jarektroyer@gmail.com>
2019-12-14 18:23:17 -0500

    auth0 rule should be done

--- src/js/_rules/rule1.js
+++ src/js/_rules/rule1.js
@@ -1,58 +1,51 @@
 function test(user, context, callback) {
     let nameSpace = "http://localhost:9000/";
-      let CB = (err, response, body, cb) => {
-          console.log("\n----->>>>\n", err, body, "\n----->>>>\n");
-          if (response && response.statusCode === 200) {
+    // ---
+    let count = 0;
+    let CB = (err, response, body, cb) => {
+        console.log(++count, "\n----->>>>\n", err, body, "\n----->>>>\n");
+        if (response && response.statusCode === 200) {
           context.idToken[nameSpace + "pt"] = body.auth_id;
-        callback(null, user, context);
-      }
+          callback(null, user, context);
+        }
       else if (response.finished && !err) cb();
-    };
-    
-    // --- 
-    
-      let loginReq = {
-          headers: {
+    };    
+    // ---
+    let loginReq = {
+        headers: {
           "Content-Type":"application/json",
           "Accept": "vnd.example.v2"
-          },
-          url: "http://api.elephant.spartahack.com/sessions",
-          body: {
+        },
+        url: "http://api.elephant.spartahack.com/sessions",
+        body: {
           "email": user.email ? user.email : "null",
-          "ID_Token": context.query ? context.query.state : "null"
-          },
-          json: true
-      };
-    
-    // --- 
-      
+          "ID_Token": context.clientID
+        },
+        json: true
+    };    
     let createReq = {
-          headers: {
-          "Content-Type":"application/json",
+      headers: {
+        "Content-Type":"application/json",
       },
       url: "http://api.elephant.spartahack.com/users",
       body: {
-          "email": user.eamil,
-           "first_name": user.given_name,
+        "email": user.email,
+        "first_name": user.given_name,
         "last_name": user.family_name,
-        "ID_Token": context.query ? context.query.client_id : "null"
+        "auth_id": user.user_id,
+        "ID_Token": context.clientID
       },
       json: true
     };
-    
-    // --- 
-      
-      let loginCb = (err, response, body) => CB(err, response, body, 
-          () => callback(new Error('Invalid User'))); 
+    // ---
+    let loginCb = (err, response, body) => CB(err, response, body, 
+        () => callback(new Error('Invalid User'))); 
     
     let createCb = (err, response, body) => 
         CB(err, response, body, () => request.post(loginReq, loginCb));
-    // console.log(user.email, '\n ---- \n', context.query);
-
-    console.log("!___====", context);
-    // ---   
+    // ---
+    // console.log(user);
+    // console.log("\n>>>>\n", user,"\n##^^^^##\n", context,"\n>>>>\n");
+    console.log("\n>>>>\n", loginReq,"\n##^^^^##\n", createReq,"\n>>>>\n");
     request.post(createReq, createCb);
-      //console.log("\n>>>>\n", user,"\n##^^^^##\n", context,"\n>>>>\n");
-    //callback(null, user, context);
-     
   }
