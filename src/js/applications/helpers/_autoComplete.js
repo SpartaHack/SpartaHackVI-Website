c74c87c0553909d26f3c807e17686eb384a44552
Jarek Troyer <jarektroyer@gmail.com>
2020-01-17 23:43:27 -0500

    auto complete done

--- src/js/applications/helpers/_autoComplete.js
+++ src/js/applications/helpers/_autoComplete.js
@@ -1,4 +1,134 @@
 let request = require('request')
+
+class autoCompeteInput {
+    constructor(filterSrc, components) {
+        this.components = components
+        this.filterSrc = filterSrc
+        this.curInd = 0
+
+        this.itemWrap = document.createElement('span')
+        this.itemWrap.className = 'autocomplete-list'
+        this.currentItems = document.createElement('ul')
+        this.itemWrap.append(this.currentItems)
+        
+        this.components.input.addEventListener('keyup', e => this.route(e.keyCode))
+    }
+
+    hide() {
+        if (this.components.wrap.lastChild === this.itemWrap)
+            this.components.wrap.removeChild(this.itemWrap)
+
+        let show = () => {
+            this.show()
+
+            this.components.wrap.removeEventListener('focus', show)
+            this.components.wrap.removeEventListener('mouseenter', show)
+        }
+
+        this.components.input.addEventListener('focus', show)
+        this.components.wrap.addEventListener('mouseenter', show)
+    }
+
+    show() {
+        if (!this.currentItems.childElementCount) 
+            this.hide()
+
+        else if (this.components.wrap.lastChild.className === 'autocomplete-list')
+            this.components.wrap.replaceChild(this.itemWrap, this.components.wrap.lastChild)
+
+        else this.components.wrap.appendChild(this.itemWrap)
+
+        let hide = () => {
+            this.hide()
+
+            this.components.wrap.removeEventListener('blur', hide)
+            this.components.wrap.removeEventListener('mouseleave', hide)
+        }
+
+        this.components.wrap.addEventListener('blur', hide)
+        this.components.wrap.addEventListener('mouseleave', hide)
+    }
+
+    set currentIndex(val) {
+        let current = this.currentItems.childNodes[this.curInd]
+        if (current) current.className = ''
+
+        let curCount = this.currentItems.childElementCount    
+        val = (!Number.isInteger(val) || val >= curCount) ? 0 :
+            (val < 0 ? curCount - 1 : val)
+    
+        this.currentItems.childNodes[val].className = 'active-auto'
+        this.curInd = val
+    }
+
+    route(keycode) {
+        switch (keycode) {
+            case 40: 
+            this.currentIndex = this.curInd + 1
+            break
+            case 38: 
+            this.currentIndex = this.curInd - 1
+            break
+            case 13: 
+            this.select(this.currentItems.childNodes[this.curInd])
+            break
+            default: 
+            this.filter()
+        }
+    }
+
+    filter() {        
+        this.clear()
+        if (this.components.input.value.length < 3) 
+            return
+
+        let query = this.components.input.value.toLowerCase()
+        let first = true
+
+        this.filterSrc.forEach(potRes => {
+                if (potRes.toLowerCase().search(query) >= 0) {
+                    let thisOpt = document.createElement('li')
+                    if (first) {
+                        thisOpt.className = 'active-auto'
+                        first = false
+                    }
+
+                    thisOpt.appendChild(document.createElement('p'))
+                    thisOpt.firstChild.innerHTML = potRes
+
+                    thisOpt.addEventListener('click', 
+                        e => this.select(thisOpt) )
+                    
+                    this.currentItems.appendChild(thisOpt)
+                }
+            })
+
+        if (!first) this.show()
+    }
+
+    clear() { this.currentItems.innerHTML = '' }
+
+    showActive() {
+        this.currentItems.childNodes[this.curInd].className = "active-auto"
+    }
+    hideActive() {
+        this.currentItems.childNodes[this.curInd].className = ""
+    }
+
+    select(item) {
+        if (!item) return
+
+        let updated = this.components.input
+        updated.value = item.firstChild.innerHTML
+
+        this.components.wrap.replaceChild(updated, this.components.input)
+        this.components.input = updated
+
+        if (this.components.wrap.lastChild === this.itemWrap)
+            this.components.wrap.removeChild(this.itemWrap)
+    }   
+}
+
 let filterIndex = {
     'major': "/data/majors-set.json",
     'city': "/data/cities-set.json",
@@ -10,12 +140,11 @@ let filterKeyIndex = {
     'unis': 'name'
 }
 
-let getDict = (which, cb) => {
-    console.log(which, 'cool')
+let ready = (appHandler, components, args) => {
     let dictRq = {
         headers: 
             { "Content-Type":"application/json" },
-        url: window.location.origin + filterIndex[which],
+        url: window.location.origin + filterIndex[args.name],
         json: true
     }
 
@@ -23,109 +152,11 @@ let getDict = (which, cb) => {
         let autoCompleteHandler
         if (response && response.statusCode === 200 
             && Array.isArray(body) ) 
-            autoCompleteHandler = cb(body)
+            autoCompleteHandler = new autoCompeteInput(body, components)
     }
-
-    request.get(dictRq, dictCb)
-}
-
-// Creates our auto suggest unordered list/wrap
-let filter = (filterSrc, cmpFnc, components) => {
-    if (!Array.isArray(filterSrc)) return
-    else if (!cmpFnc || !components 
-        || components.input.value.length < 3) return false
-
-    let listWrap = document.createElement('span')
-    listWrap.className = 'autocomplete-list'
-
-    let list = document.createElement('ul')
-    listWrap.appendChild(list)
-    
-    filterSrc.forEach(target => {
-        let result = cmpFnc(target) 
-        if (result) { // update list
-            let thisOpt = document.createElement('li')
-            thisOpt.addEventListener('click', 
-                e => selectItem(thisOpt, components))
-            thisOpt.innerHTML = "<p>" + result + "</p>"
-            list.appendChild(thisOpt)
-
-        }
-    })
-        
-    return startDom(listWrap, components)
-}   
-
-class autoCompeteInput {
-    constructor(filterSrc, components) {
-        this.components = components
-        this.filterSrc = fiterSrc
-
-        this.curInd
-        this.itemWrap = document.createElement('span')
-        this.itemWrap.className = 'autocomplete-area'
-        this.currentItems = document.createElement('ul')
-
-        this.components.input.addEventListener('keyup', 
-            e => this.route(e.keyCode) )
-
-        let hide = () => this.hide()
-        let show = () => this.show()
-        this.components.wrap.addEventListener('blur', hide)
-        this.components.wrap.addEventListener('mouseleave', hide)
-        this.components.wrap.addEventListener('mouseenter', show)
-        this.components.input.addEventListener('focus', show)
-    }
-
-    hide() {
-        if (this.components.wrap.lastChild === this.itemWrap)
-            this.components.wrap.removeChild(this.itemWrap)
-    }
-
-    show() {
-        if (!this.currentItems.childElementCount) 
-            this.hide()
-
-        else if (this.components.wrap.lastChild.className === 'autocomplete-area')
-            this.components.wrap.replaceChild(this.itemWrap, this.components.wrap.lastChild)
-
-        else this.components.wrap.appendChild(this.itemWrap)
-    }
-
-    get current() {
-        this.currentItems.childElementCount > this.currentItems 
-            ? this.currentItems.childNodes[this.currentIndex] : undefined
-    }
-
-    set currentIndex(val) {
-        val = val > this.currentItems.childElementCount ? 0 :
-            (val < 0 ? this.currentItems.childElementCount - 1 : val)
-        
-        this.currentItems.childNodes[this.curInd] = ''
-        this.curInd = val
-        this.currentItems.childNodes[val].className = 'active'
-    }
-
-    route(keycode) {
-        switch (keycode) {
-            case 40: this.currentIndex = this.curInd + 1
-            break
-            case 38: this.currentIndex = this.curInd - 1
-            break
-            case 13: this.select(this.current)
-        }
-    }
-
-    filter() {
-
-    }
-}
-
-let ready = (appHandler, components, args) => {
-    let wrapper = options => new autoCompeteInput(options, components)
 
     let dictInit = e => {
-        getDict(args.name, wrapper )
+        request.get(dictRq, dictCb)
         components.input.removeEventListener('focus', dictInit)
     }
     components.input.addEventListener('focus', dictInit)
