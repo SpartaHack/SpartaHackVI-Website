83d6d2c2cb32ac3bf5fa743b247e2f7d62afdbe1
Jarek Troyer <jarektroyer@gmail.com>
2020-02-14 21:27:24 -0500

    different logging

--- src/js/req.js
+++ src/js/req.js
@@ -1,6 +1,6 @@
 const request = require('request'),
 baseUrl = require('./../../env.json').baseUrl
-console.log(baseUrl, request, module.exports.uest)
 
 module.exports.uest = request
 module.exports.base = baseUrl
+console.log(baseUrl, request, module.exports.uest)
