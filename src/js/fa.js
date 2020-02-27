e593800bfba1970a87daab954ccf16807e329435
Jarek Troyer <jarektroyer@gmail.com>
2020-02-27 18:03:04 -0500

    updated dietary restrictions

--- src/js/fa.js
+++ src/js/fa.js
@@ -1,13 +1,10 @@
-// require.context('./../../assets')
-
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faSearch, faTimes, faCheck, faChevronCircleRight, faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+
 let fontAwesome = () => {
     library.add(faSearch, faTimes, faCheck, faChevronCircleRight, faPlusSquare)
-    console.log(library)
     dom.i2svg()
 }
-
 export default fontAwesome
