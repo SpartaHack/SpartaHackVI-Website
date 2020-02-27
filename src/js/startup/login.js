e91a9e3081327e3d86b6565df41a9975e744f2a2
Jarek Troyer <jarektroyer@gmail.com>
2020-02-27 04:39:26 -0500

    login/page initiation restored

---
+++ src/js/startup/login.js
@@ -0,0 +1,19 @@
+const authConfig = require('./user_config').default,
+eventState = require('./eventState').default
+
+let login = async cb => {
+    cb = Array.isArray(cb) ? cb : [cb]
+
+    let after = (auth, user) =>
+        eventState(state => 
+            cb.forEach(func => func(auth, user, state)) )
+
+    let auth = await authConfig(after),
+    logoutButton = document.getElementById('nav-logout')
+
+    if (logoutButton)
+        logoutButton.addEventListener('click', e => auth.logout())
+    
+    return
+}
+module.exports.default = login
---
+++ src/js/startup/user_config.js
@@ -0,0 +1,50 @@
+const auth0 = require('auth0-js').default.WebAuth,
+transactions = require('./../transactions'),
+env = require('./../../../env.json')
+
+async function auth_func(cb) {
+    let args = window.location.hash, 
+    auth = await new auth0(env.auth),
+    key = transactions.getKey()
+
+    if (!args && !key) auth.authorize()
+
+    else if (args) auth.parseHash({}, (err, hashedInfo) => {
+        let payload = hashedInfo.idTokenPayload,
+        getUserItem = name => 
+            payload[window.location.origin+"/"+name],
+        userItems = ['pt', 'aid', 'rsvp'],
+        userOut = {
+            'email': payload.email,
+            'oaid': payload.sub,
+            'name': payload.name != payload.email
+                ? payload.name : undefined,
+            'exp': payload.exp,
+            'picture': payload.picture,
+            'github': payload.sub.substr(0,6) == "github" 
+                ? payload.nickname : undefined
+        }
+
+        userItems.forEach(
+            i => userOut[i] = getUserItem(i) )
+
+        window.sessionStorage.setItem('st',
+            transactions.getKey() )
+
+        transactions.userOut(userOut)
+        history.replaceState(null, null, ' ')
+
+        cb(auth, userOut)
+    } )
+    else {
+        let user = transactions.userIn()
+        if (Date.now()/1000 - user.exp < 600)
+            auth.authorize()
+
+        cb(auth, user)
+    }
+
+    return auth
+}
+
+export default auth_func
